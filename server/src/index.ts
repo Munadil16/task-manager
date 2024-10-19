b2b5236cd145993e76b7d5e63b5e080d4788f449
Munadil16 <munadilxd@gmail.com>
2024-10-19 23:40:56 +0530

    feat: Add user routes, model, and controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,19 @@ import dotenv from "dotenv";
 import express from "express";
 import type { Application, NextFunction, Request, Response } from "express";
 
+import { connectDb } from "./db";
+import userRouter from "./routes/user.routes";
+
 dotenv.config();
+
+connectDb();
 
 const app: Application = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
 
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err.stack);
